dfa9d970ba1d882f0e60f7bd6167b5175c48535c
Rodrigo <rodrigonpi@hotmail.com>
2022-08-03 21:21:44 -0300

    exercicio resolvido

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,46 @@
+import React, {useState} from "react";
+
 const App = () => {
+  const [nome, setNome] = useState("");
+  const [sobrenome, setSobrenome] = useState("");
+  const [idade, setIdade] = useState("");
+
+  const handleInputNome = (event: React.ChangeEvent<HTMLInputElement>)=>{
+    setNome(event.target.value);
+  }
+
+  const handleInputSobrenome = (event: React.ChangeEvent<HTMLInputElement>)=>{
+    setSobrenome(event.target.value);
+  }
+
+  const handleInputIdade = (event: React.ChangeEvent<HTMLInputElement>)=>{
+    setIdade(event.target.value);
+  }
+
   return (
     <div>
       <div>
         Nome:
-        <input type="text" />
+        <input type="text"value={nome} onChange={handleInputNome}/>
       </div>
 
       <div>
         Sobrenome:
-        <input type="text" />
+        <input type="text"value={sobrenome} onChange={handleInputSobrenome}/>
       </div>
 
       <div>
         Idade:
-        <input type="text" />
+        <input type="text" value={idade} onChange={handleInputIdade} />
       </div>
 
       <hr/>
 
-      Olá ---, tudo bem?<br/>
-      Você tem -- anos.
+      Olá {nome} {sobrenome}, tudo bem?<br/>
+      Você tem {idade} anos.
     </div>
   );
-}
+  }
+
 
 export default App;
